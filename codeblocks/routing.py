dfc3c99559adf356e7d9d2a75e0662b31357b9b9
eyalyehiely <eyalwork0@gmail.com>
2024-07-29 18:34:54 +0300

    fix syntax error in routing.py

--- codeblocks/routing.py
+++ codeblocks/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/codeblock/(?P<codeblock_id>\w+)/$', consumers.CodeBlockConsumer.as_asgi()),
+    re_path(r'ws/codeblock/(?P<codeblock_id>\d+)/$', consumers.CodeBlockConsumer.as_asgi()),
 ]
