6e19e43d67fa38eb44bae5a0afce46d6bc0eeb9b
eyalyehiely <eyalwork0@gmail.com>
2024-07-29 18:37:24 +0300

    fixing import in asgi file

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import codeblocks
+import codeblocks.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
@@ -10,7 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            codeblocks.routing.websocket_urlpatterns 
+            codeblocks.routing.websocket_urlpatterns
         )
     ),
 })
