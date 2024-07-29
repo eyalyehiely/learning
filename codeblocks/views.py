d1913f558714533230b983801eb620eb49d101b6
eyalyehiely <eyalwork0@gmail.com>
2024-07-29 18:34:18 +0300

    use serializer in get code block view

--- codeblocks/views.py
+++ codeblocks/views.py
@@ -24,15 +24,14 @@ def get_code_block(request, code_block_id):
     try:
         codeBlock = CodeBlock.objects.get(id=code_block_id)
         logger.debug(f"CodeBlock {codeBlock.title} provided to the client.")
-        return Response({
-            'id': codeBlock.id,
-            'title': codeBlock.title,
-            'code': codeBlock.code
-        }, status=200)
+        serializer = CodeBlockSerializer(codeBlock)
+
+        return Response(serializer.data, status=200)
     
     except ObjectDoesNotExist:
         logger.error(f"CodeBlock with id {code_block_id} not found.")
         return Response({'Error': 'Code block not found'}, status=404)
+    
     except Exception as e:
         logger.error(f"Error retrieving code block: {e}")
         return Response({'Error': 'An error occurred'}, status=500)
