439055c0e6374e82356ba4f9a8ac85c311ca7818
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 01:10:35 +0300

    add submission routes - codeblock_submission_detail & codeblock_submission

--- codeblocks/urls.py
+++ codeblocks/urls.py
@@ -1,10 +1,11 @@
-
 from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('codeblocks/',get_code_blocks, name="code_locks"),
-    path('codeblock/<int:code_block_id>/',get_code_block,name="get code block"),
-    path('codeblock/<int:code_block_id>/check/',check_user_code,name="check_user_code"),
+    path('codeblocks/', get_code_blocks, name="code_locks"),
+    path('codeblock/<int:code_block_id>/', get_code_block, name="get code block"),
+    path('codeblock/<int:code_block_id>/check/', check_user_code, name="check_user_code"),
     path('codeblock/<int:code_block_id>/edit/', edit_code_block, name='edit_code_block'),
+    path("codeblock/submission/<uuid:user_id>/", codeblock_submission, name="codeblock_submission"),
+    path("codeblock/submission/<int:submission_id>/", codeblock_submission_detail, name="codeblock_submission_detail"),
 ]
--- codeblocks/views.py
+++ codeblocks/views.py
@@ -1,13 +1,14 @@
 import logging
+
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import CodeBlockSerializer
-from .models import CodeBlock
+
 from django.core.exceptions import ObjectDoesNotExist
 from .utils import send_codeblock_update
 from drf_yasg.utils import swagger_auto_schema
-from .models import CodeBlock
-from .serializers import CodeBlockSerializer
+from .models import *
+from .serializers import *
 from drf_yasg import openapi
 
 logger = logging.getLogger('codeBlocks')
@@ -35,21 +36,20 @@ responses = {
 }
 
 
-
 @swagger_auto_schema(method='get', responses={200: CodeBlockSerializer(many=True)})
 @api_view(['GET'])
 def get_code_blocks(request):
     try:
         codeBlocks = CodeBlock.objects.all()
         logger.debug("All code blocks provided to the client.")
-        all_code_blocks = [{'id': block.id, 'title': block.title, 'instructions': block.instructions, 'code': block.code} for block in codeBlocks]
+        all_code_blocks = [
+            {'id': block.id, 'title': block.title, 'instructions': block.instructions, 'code': block.code} for block in
+            codeBlocks]
         return Response({'codeBlocks': all_code_blocks}, status=200)
-    
+
     except Exception as e:
         logger.error(f"Error retrieving code blocks: {e}")
         return Response({'Error': 'Page not found'}, status=404)
-
-
 
 
 @api_view(['GET'])
@@ -60,18 +60,14 @@ def get_code_block(request, code_block_id):
         serializer = CodeBlockSerializer(codeBlock)
 
         return Response(serializer.data, status=200)
-    
+
     except ObjectDoesNotExist:
         logger.error(f"CodeBlock with id {code_block_id} not found.")
         return Response({'Error': 'Code block not found'}, status=404)
-    
+
     except Exception as e:
         logger.error(f"Error retrieving code block: {e}")
         return Response({'Error': 'An error occurred'}, status=500)
-
-
-
-
 
 
 # edit code
@@ -83,24 +79,23 @@ def edit_code_block(request, code_block_id):
         code = request.data.get('code')
         codeBlock.code = code
         codeBlock.save()
-        
+
         # Send update to the mentor group if the request is from a student
-        if request.user.role == 'student':  # Assume you have a user role attribute
-            send_codeblock_update(code_block_id, code)
+        # if request.user.role == 'student':  # Assume you have a user role attribute
+        send_codeblock_update(code_block_id, code)
 
         logger.info(f"CodeBlock {codeBlock.title} updated.")
         return Response({"success": "Code block updated"}, status=200)
-    
+
     except CodeBlock.DoesNotExist:
         logger.error(f"No CodeBlock found with ID {code_block_id}.")
         return Response({"error": f"No CodeBlock found with ID {code_block_id}."}, status=404)
-    
+
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         return Response({"error": f"An error occurred: {e}"}, status=500)
-    
 
-    
+
 @api_view(['POST'])
 def check_user_code(request, code_block_id):
     try:
@@ -109,21 +104,77 @@ def check_user_code(request, code_block_id):
         if not script_solution:
             logger.error("No solution found for the given script ID.")
             return Response({"error": "No solution found for the given script ID."}, status=404)
-        
+
         if user_script == script_solution:
             logger.info("User script matches the solution.")
             send_codeblock_update(code_block_id, user_script)
             return Response({"success": "match"}, status=200)
-        
+
         else:
             logger.info("User script does not match the solution.")
             send_codeblock_update(code_block_id, user_script)
             return Response({"fail": "not match"}, status=200)
-        
+
     except CodeBlock.DoesNotExist:
         logger.error(f"No CodeBlock found with ID {code_block_id}.")
         return Response({"error": f"No CodeBlock found with ID {code_block_id}."}, status=404)
-    
+
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         return Response({"error": f"An error occurred: {e}"}, status=500)
+
+
+@swagger_auto_schema(method='post', request_body=SubmissionSerializer)
+@api_view(['POST'])
+def codeblock_submission(request, user_id):
+    if request.method == 'POST':
+        codeblock_id = request.data.get('code_block_id')
+
+        codeblock = get_object_or_404(CodeBlock, id=codeblock_id)
+
+        check_submission = Submission.objects.filter(code_block=codeblock, user_id=user_id)
+        if check_submission.exists():
+            return Response({'error': 'Submission already exists'}, status=400)
+
+        else:
+            serializer = SubmissionSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=201)
+
+            return Response(serializer.errors, status=400)
+
+
+def create_new_submission(data):
+   # Create a new submission
+    pass
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def codeblock_submission_detail(request,code_block_id):
+
+    codeblock = get_object_or_404(CodeBlock, id=code_block_id)
+
+    if request.method == "GET":
+        user_id =  request.data.get('user_id')
+        submission = Submission.objects.filter(code_block_id=codeblock.id, user_id=user_id)
+
+        if submission.exists():
+            serializer = SubmissionSerializer(submission, many=True)
+            return Response(serializer.data, status=200)
+
+        else:
+            return Response({'error': 'No submission found'}, status=400)
+
+
+    elif request.method == "PUT":
+        user_id =  request.data.get('user_id')
+        submission = Submission.objects.filter(code_block_id=codeblock.id, user_id=user_id)
+        if submission.exists():
+            serializer = SubmissionSerializer(submission, data=request.data)
+            if serializer.is_valid():
+                new_sub = serializer.save()
+                send_codeblock_update(code_block_id,new_sub.user_code)
+                return Response(serializer.data, status=200)
+            return Response(serializer.errors, status=400)
+        else:
+            return Response({'error': 'No submission found'}, status=400)
