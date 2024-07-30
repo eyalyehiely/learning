10025fdbd820421db652dbf610e15053a5a32b04
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 01:10:55 +0300

    add SubmissionSerializer

--- codeblocks/serializers.py
+++ codeblocks/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import CodeBlock
+from .models import CodeBlock, Submission
+
 
 class CodeBlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = CodeBlock
         fields = '__all__'
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Submission
+        exclude = ['user_code']
+
+    def create(self, validated_data):
+        submission = Submission.objects.create(**validated_data)
+        submission.save()
+        submission.user_code = submission.code_block.code
+        return submission
