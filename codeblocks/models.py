52f7f1c678bae353c26c6333547e9913d7a3cf93
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 01:09:54 +0300

    add submission model

--- codeblocks/models.py
+++ codeblocks/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class CodeBlock(models.Model):
-    id = models.IntegerField(primary_key=True, editable=False,)
+    id = models.IntegerField(primary_key=True, editable=False, )
     title = models.CharField(max_length=100)
     instructions = models.TextField()
     code = models.TextField()
-    solution = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+
+class Submission(models.Model):
+    code_block = models.ForeignKey(CodeBlock, on_delete=models.CASCADE)
+    user_code = models.TextField()
+    user = models.CharField(max_length=100)
+    passed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
