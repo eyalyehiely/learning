d6e033fe57ca62338e65b246fabcfa651a521701
eyalyehiely <eyalwork0@gmail.com>
2024-07-30 14:13:10 +0300

    adding instructions property to codeBlock model

--- codeblocks/models.py
+++ codeblocks/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class CodeBlock(models.Model):
     id = models.IntegerField(primary_key=True, editable=False,)
     title = models.CharField(max_length=100)
+    instructions = models.TextField()
     code = models.TextField()
     solution = models.TextField(blank=True, null=True)
 
