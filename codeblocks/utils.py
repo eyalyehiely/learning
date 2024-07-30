924f50761e99bcab1fad6fd32943920739c7ca4f
eyalyehiely <eyalwork0@gmail.com>
2024-07-30 14:16:16 +0300

    add utils.py

---
+++ codeblocks/utils.py
@@ -0,0 +1,15 @@
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+def send_codeblock_update(codeblock_id, code):
+    channel_layer = get_channel_layer()
+    group_name = f'codeblock_{codeblock_id}'
+
+    # Send message to room group
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        {
+            'type': 'code_update',
+            'code': code
+        }
+    )
