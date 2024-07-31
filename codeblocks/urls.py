48f350669b66e97c035965f99aac8d9677576c8b
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 01:25:22 +0300

    add visitor & current uuid routes

--- codeblocks/urls.py
+++ codeblocks/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import get_code_blocks, check_user_code, codeblock_submission, codeblock_submission_detail
+from .views import *
 
 urlpatterns = [
     path('codeblocks/', get_code_blocks, name="code_locks"),
     path('codeblock/<int:code_block_id>/check/', check_user_code, name="check_user_code"),
-    path("codeblock/submission/<uuid:user_id>/", codeblock_submission, name="codeblock_submission"),
+    path("codeblock/submission/", codeblock_submission, name="codeblock_submission"),
     path("codeblock/submission/<int:code_block_id>/", codeblock_submission_detail, name="codeblock_submission_detail"),
+    path("fetchClientUuidtoServer/", fetch_client_uuid_to_server, name='fetch_client_uuid_to_server'),
+    path('log_visitor/', log_visitor, name='log_visitor'),
+
 ]
