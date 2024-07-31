73007cd30f143666e912c591e892e3d47ee1875a
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 13:04:44 +0300

    add submission logger

--- codeblocks/views.py
+++ codeblocks/views.py
@@ -11,7 +11,8 @@ from .models import *
 from .serializers import *
 from drf_yasg import openapi
 
-logger = logging.getLogger('codeBlocks')
+codeblock_logger = logging.getLogger('code_block')
+submission_logger = logging.getLogger('submission')
 
 # Define the code parameter in the request body
 # Define the code parameter in the request body
@@ -41,14 +42,14 @@ responses = {
 def get_code_blocks(request):
     try:
         codeBlocks = CodeBlock.objects.all()
-        logger.debug("All code blocks provided to the client.")
+        codeblock_logger .debug("All code blocks provided to the client.")
         all_code_blocks = [
             {'id': block.id, 'title': block.title, 'instructions': block.instructions, 'code': block.code} for block in
             codeBlocks]
         return Response({'codeBlocks': all_code_blocks}, status=200)
 
     except Exception as e:
-        logger.error(f"Error retrieving code blocks: {e}")
+        codeblock_logger .error(f"Error retrieving code blocks: {e}")
         return Response({'Error': 'Page not found'}, status=404)
 
 
@@ -56,17 +57,17 @@ def get_code_blocks(request):
 def get_code_block(request, code_block_id):
     try:
         codeBlock = CodeBlock.objects.get(id=code_block_id)
-        logger.debug(f"CodeBlock {codeBlock.title} provided to the client.")
+        codeblock_logger .debug(f"CodeBlock {codeBlock.title} provided to the client.")
         serializer = CodeBlockSerializer(codeBlock)
 
         return Response(serializer.data, status=200)
 
     except ObjectDoesNotExist:
-        logger.error(f"CodeBlock with id {code_block_id} not found.")
+        codeblock_logger .error(f"CodeBlock with id {code_block_id} not found.")
         return Response({'Error': 'Code block not found'}, status=404)
 
     except Exception as e:
-        logger.error(f"Error retrieving code block: {e}")
+        codeblock_logger .error(f"Error retrieving code block: {e}")
         return Response({'Error': 'An error occurred'}, status=500)
 
 
@@ -80,19 +81,17 @@ def edit_code_block(request, code_block_id):
         codeBlock.code = code
         codeBlock.save()
 
-        # Send update to the mentor group if the request is from a student
-        # if request.user.role == 'student':  # Assume you have a user role attribute
         send_codeblock_update(code_block_id, code)
 
-        logger.info(f"CodeBlock {codeBlock.title} updated.")
+        codeblock_logger .info(f"CodeBlock {codeBlock.title} updated.")
         return Response({"success": "Code block updated"}, status=200)
 
     except CodeBlock.DoesNotExist:
-        logger.error(f"No CodeBlock found with ID {code_block_id}.")
+        codeblock_logger .error(f"No CodeBlock found with ID {code_block_id}.")
         return Response({"error": f"No CodeBlock found with ID {code_block_id}."}, status=404)
 
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        codeblock_logger .error(f"An error occurred: {e}")
         return Response({"error": f"An error occurred: {e}"}, status=500)
 
 
@@ -102,26 +101,31 @@ def check_user_code(request, code_block_id):
         user_script = request.data.get('code')
         script_solution = CodeBlock.objects.get(id=code_block_id).solution
         if not script_solution:
-            logger.error("No solution found for the given script ID.")
+            codeblock_logger .error("No solution found for the given script ID.")
             return Response({"error": "No solution found for the given script ID."}, status=404)
 
         if user_script == script_solution:
-            logger.info("User script matches the solution.")
+            codeblock_logger .info("User script matches the solution.")
             send_codeblock_update(code_block_id, user_script)
             return Response({"success": "match"}, status=200)
 
         else:
-            logger.info("User script does not match the solution.")
+            codeblock_logger .info("User script does not match the solution.")
             send_codeblock_update(code_block_id, user_script)
             return Response({"fail": "not match"}, status=200)
 
     except CodeBlock.DoesNotExist:
-        logger.error(f"No CodeBlock found with ID {code_block_id}.")
+        codeblock_logger .error(f"No CodeBlock found with ID {code_block_id}.")
         return Response({"error": f"No CodeBlock found with ID {code_block_id}."}, status=404)
 
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        codeblock_logger .error(f"An error occurred: {e}")
         return Response({"error": f"An error occurred: {e}"}, status=500)
+
+
+
+
+
 
 
 @swagger_auto_schema(method='post', request_body=SubmissionSerializer)
@@ -135,7 +139,8 @@ def codeblock_submission(request, user_id):
         check_submission = Submission.objects.filter(code_block=codeblock, user_id=user_id)
         if check_submission.exists():
             return Response({'error': 'Submission already exists'}, status=400)
-
+        
+        # create submission if not exist
         else:
             serializer = SubmissionSerializer(data=request.data)
             if serializer.is_valid():
@@ -145,27 +150,36 @@ def codeblock_submission(request, user_id):
             return Response(serializer.errors, status=400)
 
 
-def create_new_submission(data):
-   # Create a new submission
-    pass
+# def create_new_submission(data):
+#    # Create a new submission
+#     pass
+
+
+
+
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def codeblock_submission_detail(request,code_block_id):
 
+    # check if code block exist
     codeblock = get_object_or_404(CodeBlock, id=code_block_id)
+
 
     if request.method == "GET":
         user_id =  request.data.get('user_id')
         submission = Submission.objects.filter(code_block_id=codeblock.id, user_id=user_id)
 
+        #check if there is already submission exist
         if submission.exists():
             serializer = SubmissionSerializer(submission, many=True)
+            submission_logger("submission exist")
             return Response(serializer.data, status=200)
 
         else:
             return Response({'error': 'No submission found'}, status=400)
 
-
+    # saving the new submission
     elif request.method == "PUT":
         user_id =  request.data.get('user_id')
         submission = Submission.objects.filter(code_block_id=codeblock.id, user_id=user_id)
@@ -173,8 +187,9 @@ def codeblock_submission_detail(request,code_block_id):
             serializer = SubmissionSerializer(submission, data=request.data)
             if serializer.is_valid():
                 new_sub = serializer.save()
+                submission_logger("submission saved")
                 send_codeblock_update(code_block_id,new_sub.user_code)
                 return Response(serializer.data, status=200)
             return Response(serializer.errors, status=400)
         else:
-            return Response({'error': 'No submission found'}, status=400)
+            codeblock_submission(request, user_id)
