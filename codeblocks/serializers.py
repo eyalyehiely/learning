a5cf5716c240f4b03272b0c8497e37ecd9dc5b9a
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 01:25:39 +0300

    improve submission serializer

--- codeblocks/serializers.py
+++ codeblocks/serializers.py
@@ -1,20 +1,21 @@
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 from .models import CodeBlock, Submission
-
 
 class CodeBlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = CodeBlock
-        fields = '__all__'
-
+        fields = ['id', 'title', 'instructions', 'code']
 
 class SubmissionSerializer(serializers.ModelSerializer):
+    code_block_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = Submission
-        exclude = ['user_code']
+        fields = ['code_block_id', 'user_id', 'user_code', 'passed', 'created_at']
 
     def create(self, validated_data):
-        submission = Submission.objects.create(**validated_data)
-        submission.save()
-        submission.user_code = submission.code_block.code
+        code_block_id = validated_data.pop('code_block_id')
+        code_block = get_object_or_404(CodeBlock, id=code_block_id)
+        submission = Submission.objects.create(code_block=code_block, **validated_data)
         return submission
