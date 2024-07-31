4a6baab6402c76e5c3c2fcaeee1166099990938a
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 19:18:24 +0300

    fixing utils.py

--- codeblocks/utils.py
+++ codeblocks/utils.py
@@ -1,15 +1,13 @@
+# utils.py
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
-def send_codeblock_update(codeblock_id, code):
+def send_codeblock_update(code_block_id, code):
     channel_layer = get_channel_layer()
-    group_name = f'codeblock_{codeblock_id}'
-
-    # Send code to room group
     async_to_sync(channel_layer.group_send)(
-        group_name,
+        f"codeblock_{code_block_id}",
         {
-            'type': 'code_update',
-            'code': code,
+            "type": "code_update",
+            "code": code,
         }
     )
