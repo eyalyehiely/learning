733c86605b855cf8a0c017b75dca8045ed0c66df
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 04:11:30 +0300

    add the edit path

--- codeblocks/urls.py
+++ codeblocks/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("codeblock/submission/", codeblock_submission, name="codeblock_submission"),
     path("codeblock/submission/<int:code_block_id>/", codeblock_submission_detail, name="codeblock_submission_detail"),
     path("fetchClientUuidtoServer/", fetch_client_uuid_to_server, name='fetch_client_uuid_to_server'),
+    path("codeblock/submission/<int:code_block_id>/edit/", edit_submission, name="edit_submission"),
     path('log_visitor/', log_visitor, name='log_visitor'),
 
 ]
