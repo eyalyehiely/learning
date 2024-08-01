ebf2806316227370b8d580eb44dd93a96a8378b4
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 08:53:08 +0300

    changing id to be autofield

--- codeblocks/models.py
+++ codeblocks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CodeBlock(models.Model):
-    id = models.IntegerField(primary_key=True, editable=False, )
+    id = models.AutoField(primary_key=True) 
     title = models.CharField(max_length=100)
     instructions = models.TextField()
     code = models.TextField()
