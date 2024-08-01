ce55c68c98610372fce7de2d43c233cad06e69fd
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 19:52:42 +0300

    add solution to admin panel

--- codeblocks/admin.py
+++ codeblocks/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from.models import *
 
 class CodeBlockAdmin(admin.ModelAdmin):
-    list_display = ('id','title','instructions','code')
+    list_display = ('id','title','instructions','code','solution')
 admin.site.register(CodeBlock, CodeBlockAdmin)
 
 
